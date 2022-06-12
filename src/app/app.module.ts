d207c174be9a59ac4bdd5edda1d3dd59d800cf5f
kitorosano <rosanoesteban2@gmail.com>
2022-06-12 15:05:14 -0300

    Fin seccion 20

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MuestraNombreComponent } from './components/muestra-nombre/muestra-nombre.component';
+import { Pagina1Component } from './pages/pagina1/pagina1.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, Pagina1Component, MuestraNombreComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
